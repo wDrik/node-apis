fb5d703d4e6beffd53b2b6d10a5e799c964a92d1
Iorgen Wildrik <w.drik@outlook.com>
2018-04-28 17:37:20 -0300

    Normalize Port

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log('Hello world!')
+const http      = require('http')
+const debug     = require('debug')('nodeapi:server')
+const express   = require('express')
+
+const app   = express()
+const port  = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
+
+const server = http.createServer(app)
+const router = express.Router()
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: 'Node Store API',
+        version: '0.0.1'
+    })
+})
+
+app.use('/', route)
+server.listen(port)
+console.log(`Server is running on the this http://localhost:${port}`)
+
+function normalizePort(val) {
+    const port = parseInt(val, 10)
+
+    if(isNaN(port)) {
+        return val
+    }
+
+    if(port >= 0) {
+        return port
+    }
+
+    return false
+}
