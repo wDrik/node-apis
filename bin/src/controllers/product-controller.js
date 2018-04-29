496d41d2685411a235678397fe194ac45a6d8dc9
Iorgen Wildrik <w.drik@outlook.com>
2018-04-29 17:49:15 -0300

    List products by tags

--- bin/src/controllers/product-controller.js
+++ bin/src/controllers/product-controller.js
@@ -33,6 +33,18 @@ exports.getById = (req, res, next) => {
         })
 }
 
+exports.getByTag = (req, res, next) => {
+    Product.find({
+        tags: req.params.tag,
+        active: true
+    }, 'title description price slug active')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(error => {
+            res.status(400).send(error)
+        })
+}
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body)
     product.save()
