8b2cceef27fdf380ff6f99122f8ed1ff5b41d9b7
Iorgen Wildrik <w.drik@outlook.com>
2018-04-28 23:24:30 -0300

    Abstracting routes

--- bin/src/app.js
+++ bin/src/app.js
@@ -4,39 +4,14 @@ const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
 
+// Load routes
+const indexRoute = require('./routes/index-route')
+const productsRoute = require('./routes/products-route')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const index = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'Node Store API',
-        version: '0.0.2'
-    })
-})
-
-const store = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body)
-})
-
-const update = router.put('/:id', (req, res, next) => {
-    let id = req.params.id
-    res.status(200).send({ 
-        id: id, 
-        item: req.body
-    })
-})
-
-const remove = router.delete('/:id', (req, res, next) => {
-    let id = req.params.id
-    res.status(200).send({
-        id: id,
-        item: req.body
-    })
-})
-
-app.use('/', index)
-app.use('/products', store)
-app.use('/products', update)
-app.use('/products', remove)
+app.use('/', indexRoute)
+app.use('/products', productsRoute)
 
 module.exports = app
