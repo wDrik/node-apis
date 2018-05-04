e8ab350b2c1bcbb1b77211512ff3d11bbba084df
Iorgen Wildrik <w.drik@outlook.com>
2018-05-03 23:55:12 -0300

    get user info by token

--- bin/src/controllers/customer-controller.js
+++ bin/src/controllers/customer-controller.js
@@ -21,7 +21,8 @@ exports.post = async (req, res, next) => {
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
+            password: md5(req.body.password + global.SALT_KEY),
+            roles: ['user']
         });
 
         emailService.send(
@@ -56,6 +57,7 @@ exports.authenticate = async (req, res, next) => {
         }
 
         const token = await authService.generateToken({
+            id: customer._id,
             email: customer.email,
             name: customer.name
         });
--- bin/src/controllers/order-controller.js
+++ bin/src/controllers/order-controller.js
@@ -2,6 +2,7 @@
 
 const repository = require('../repositories/order-repository');
 const guid = require('guid');
+const authService = require('../services/auth-service');
 
 exports.get = async (req, res, next) => {
     try {
@@ -19,8 +20,17 @@ exports.post = async (req, res, next) => {
     // Validations here
 
     try {
+        // Recovery token
+        const token = req.body.token
+            || req.query.token
+            || req.headers['x-access-token']
+            || req.headers.authorization;
+
+        // Decode token
+        const data = await authService.decodeToken(token);
+
         await repository.create({
-            customer: req.body.customer,
+            customer: data.id,
             number: guid.raw().substring(0, 6),
             items: req.body.items
         });
