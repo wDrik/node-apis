20dea31410e7c6331305430dd5de3fb274c18ffe
Iorgen Wildrik <w.drik@outlook.com>
2018-05-05 20:28:48 -0300

    update config.js

--- bin/src/config.js
+++ bin/src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2fdfskdfleçlk';
+global.SALT_KEY = '';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb://admin:admin@ds046357.mlab.com:46357/node_api',
-    sendgridKey: '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',
-    containerConnectionString: 'SUA CONNECTION STRING'
+    connectionString: '',
+    sendgridKey: '',
+    containerConnectionString: ''
 }
